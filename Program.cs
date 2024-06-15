3cfbf8779a46ef72197c125ce45cf61e24b37070
MungaiVic <victornmungai@gmail.com>
2024-06-15 21:33:03 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of thy tasks", Version = "v1" });
+});
+var app = builder.Build();
+
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
+
+app.MapGet("/todo",  () => TodoService.GetTodos());
+app.MapGet("/todo/{id}", (int id) => TodoService.GetTodo(id));
+
+app.Run();
+
+class TodoItem
+{
+    public int Id {get; set;}
+    public string? Item {get; set;}
+    public bool IsComplete {get; set;}
+}
---
+++ Services/TodoService.cs
@@ -0,0 +1,27 @@
+namespace TodoApi.Services;
+public record Todo
+{
+    public int Id { get; set; }
+    public string? Item { get; set; }
+    public bool Status { get; set; }
+}
+
+public class TodoService
+{
+    public static List<Todo> todos = [
+        new Todo{Id=1, Item="Item 1", Status=false},
+        new Todo{Id=2, Item="Item 2", Status=false},
+        new Todo{Id=3, Item="Item 3", Status=false},
+    ];
+
+
+    public static List<Todo> GetTodos()
+    {
+        return todos;
+    }
+
+    public static Todo ? GetTodo(int id)
+    {
+        return todos.SingleOrDefault(todo => todo.Id == id);
+    }
+}
